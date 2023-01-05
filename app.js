6d1b7fc4a8ac4b95f0f77d71acfa592c8bb1d144
mrkike <diegoperez_96@hotmail.com>
2023-01-05 18:02:24 -0300

    ConsolaInteractiva

---
+++ app.js
@@ -0,0 +1,78 @@
+require('colors');
+const { guardarDB, leerDB } = require('./helpers/GuardarArchivo');
+const { inquirerMenu, 
+  pausa, leerInput, listadoTareasBorrar, confirmar, mostrarListadoChecklist } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
+
+const main = async()=> {
+ 
+    
+    let opt = '';
+
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+
+    if( tareasDB){
+      tareas.cargarTareasFromArray(tareasDB);
+    }
+
+   
+
+    do{
+       opt = await inquirerMenu();
+
+       switch (opt){
+         case'1':
+
+         const desc = await leerInput('Descripccion: ');
+         tareas.crearTarea( desc );
+         break;
+
+         case'2':
+         tareas.listadoCompleto();
+         break;
+
+         case'3':
+         tareas.listadoCompletadasPendientes();
+         break;
+
+         case'4':
+         tareas.listadoCompletadasPendientes(false);
+         break;
+          
+         case'5':
+        const ids = await mostrarListadoChecklist(tareas.listadoArr);
+        tareas.toggleCompletadas(ids);
+         break;
+        
+         case'6':
+         const id = await listadoTareasBorrar(tareas.listadoArr);
+         if (id !=='0'){
+             const ok = await confirmar('¿Está seguro?');
+             if ( ok ){
+                     tareas.borrarTarea( id );
+                     console.log('Tarea borrada');
+           }
+         }
+         break;
+       }
+
+       
+       
+      
+       guardarDB(tareas.listadoArr)
+    
+       await pausa();
+    
+    
+  
+
+    }while (opt !== '0');
+    
+
+  
+}
+
+main();
+
